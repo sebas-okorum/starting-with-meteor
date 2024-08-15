020eb54f369824d468fde573a33fdd9b5fb8fd1f
Sebastian Gómez <sebastian.gomez@okorum.com>
2024-08-15 17:41:45 -0500

    improve meteor wallet with css

--- meteor-wallet/imports/api/ContactsCollection.js
+++ meteor-wallet/imports/api/ContactsCollection.js
@@ -2,16 +2,3 @@ import {Mongo} from 'meteor/mongo';
 
 export const ContactsCollection = new Mongo.Collection('contacts');
 
-ContactsCollection.allow({
-    insert(userId, doc) {
-      return true;
-    }
-  });
-  
-export const insertContact = ({ name, email, imageURL}) => {
-    try {
-        ContactsCollection.insert({name, email, imageURL});
-    } catch (error) {
-        console.log(error);
-    }
-}
